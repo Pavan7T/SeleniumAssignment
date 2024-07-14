bec2c8f2bb64c5b9964d1ffd34fc8339dd9cc1bf
Pavan Nanaware <p1unofficial43@gmail.com>
2024-07-14 08:56:38 +0530

    Seconf seperate branch mofication

--- Assignments/tests/ToolQA_assignment_PO.java
+++ Assignments/tests/ToolQA_assignment_PO.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 //ToolQA assignment page object file
+//Modification for second branch 
 
 public class ToolQA_assignment_PO {
 	private WebDriver driver;
